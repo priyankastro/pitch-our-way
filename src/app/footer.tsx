5d93d3c28d9e34c8488f376df0a628e0b5131896
Priyanka Das <priyankastro7@gmail.com>
2023-06-10 22:35:38 +0530

    footer phase1

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -11,15 +11,36 @@ const Footer = () => {
                     </div>
                     <div className='basis-1/4 px-7'>
                         <h3 className='font-bold text-lg mb-6'>Quick Links</h3>
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/'>About Us</a></li>
-                        <li><a href='/'>Services</a></li>
-                        <li><a href='/'>Get Set Funded</a></li>
-                        <li><a href='/'>Blog</a></li>
-                        <li><a href='/'>Contact Us</a></li>
+                        <li><a href='/' className='ml-4'>Home</a></li>
+                        <li><a href='/' className='ml-4'>About Us</a></li>
+                        <li><a href='/' className='ml-4'>Services</a></li>
+                        <li><a href='/' className='ml-4'>Get Set Funded</a></li>
+                        <li><a href='/' className='ml-4'>Blog</a></li>
+                        <li><a href='/' className='ml-4'>Contact Us</a></li>
                     </div>
                     <div className='flex-auto px-7'>
                         <h3 className='font-bold text-lg mb-6'>Contact Us</h3>
+                        <div className='flex gap-4 mb-3'>
+                            <img src='/images/icon-home.png' className='mt-2 h-full'/>
+                            <div>
+                                <h3>ADDRESS:</h3>
+                                <p>Block E 1, Jhandewalan Extension, Jhandewalan, New Delhi, Delhi</p>
+                            </div>
+                        </div>
+                        <div className='flex gap-4 mb-3'>
+                            <img src='/images/icon-mail.png' className='mt-2 h-full'/>
+                            <div>
+                                <h3>EMAIL:</h3>
+                                <p>info@pitchourway.com</p>
+                            </div>
+                        </div>
+                        <div className='flex gap-4'>
+                            <img src='/images/icon-phone.png' className='mt-2 h-full'/>
+                            <div>
+                                <h3>PHONE:</h3>
+                                <p>+91-8800606116</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
